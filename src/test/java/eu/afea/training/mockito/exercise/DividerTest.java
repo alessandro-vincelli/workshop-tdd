80ab883dcda13afb17dafefeb4c5555d39edfdf8
FLIPPY\Francesco <francyspanu01@gmail.com>
2022-07-06 12:58:41 +0200

    Update-Es_1_Fra0027

--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -1,8 +1,7 @@
 package eu.afea.training.mockito.exercise;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 public class DividerTest {
 	
