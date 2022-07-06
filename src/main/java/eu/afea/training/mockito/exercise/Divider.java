bc6d15510ee0c99bedd6d64110e4ceb46a7f7e82
DESKTOP-BB6I5KB\Amore <cosimo.chiazzese@gmail.com>
2022-07-06 15:30:17 +0200

    Esercizio1 Cosimo

---
+++ src/main/java/eu/afea/training/mockito/exercise/Divider.java
@@ -0,0 +1,21 @@
+package eu.afea.training.mockito.exercise;
+
+
+
+public class Divider {
+
+	
+		
+public DivisionResult Div (Integer dividend,Integer divisor)
+{
+		
+		
+	   return new DivisionResult(dividend,divisor,dividend/divisor) ;
+		   
+	   
+		
+	
+
+}
+
+}
--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -1,5 +1,30 @@
 package eu.afea.training.mockito.exercise;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class DividerTest {
 
+	private Divider divides;
+
+	@Before
+	public void setUp() {
+
+		divides = new Divider();
+	}
+
+	@Test
+	public void testDivides() {
+
+		Integer dividend = 10;
+		Integer divisor = 2;
+		DivisionResult result = divides.Div(dividend, divisor);
+
+		assertEquals(Integer.valueOf(5), result.getResult());
+		assertEquals(Integer.valueOf(10), result.getDividend());
+		assertEquals(Integer.valueOf(2), result.getDivisor());
+
+	}
 }
