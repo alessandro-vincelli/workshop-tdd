6bd583394d1cc6ce882fa4bc169c5ed89db7ae49
LAPTOP-51345M00\hatim <hatimmes72@gmail.com>
2022-07-06 13:46:36 +0200

    divisionRis

---
+++ src/main/java/eu/afea/training/mockito/exercise/DivisionResult.java
@@ -0,0 +1,64 @@
+package eu.afea.training.mockito.exercise;
+
+public class DivisionResult {
+	 private Integer div1;
+	 private Integer div2;
+	 private Integer ris;
+	 
+	 
+	
+
+
+
+
+
+	public DivisionResult(Integer div1, Integer div2, Integer ris) {
+		super();
+		this.div1 = div1;
+		this.div2 = div2;
+		this.ris = ris;
+	}
+
+
+
+
+	public Integer getDiv1() {
+		return div1;
+	}
+
+
+
+
+	public void setDiv1(Integer div1) {
+		this.div1 = div1;
+	}
+
+
+
+
+	public Integer getDiv2() {
+		return div2;
+	}
+
+
+
+
+	public void setDiv2(Integer div2) {
+		this.div2 = div2;
+	}
+
+
+
+
+	public Integer getRis() {
+		return ris;
+	}
+
+
+
+
+	public void setRis(Integer ris) {
+		this.ris = ris;
+	}
+	
+}
