cf8499b2723f27ceeeb126b40e37f23dd6a56f58
DESKTOP-0179QH3\Cristi <cristidaniele03@gmail.com>
2022-07-06 17:40:32 +0200

    due modi diversi per gestire l'eccezione

--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -1,6 +1,7 @@
 package eu.afea.training.mockito.exercise;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.*;
 
@@ -15,35 +16,41 @@ public class DividerTest {
 	}
 
 	@Test
-	public void setupDivides() {
+	public void setupDivides() throws ImpossibleCalculationException {
 		Integer dividend = 10;
 		Integer divisor = 2;
-	
 
 		DividerResult result = divider.divides(dividend, divisor);
 		assertEquals(Integer.valueOf(5), result.getResult());
 		assertEquals(Integer.valueOf(10), result.getDivident());
 		assertEquals(Integer.valueOf(2), result.getDivisor());
 	}
+
 	@Test
 	public void testDivides_divisionByZero() {
 		Integer dividend = 10;
 		Integer divisor = 0;
-		
 
-		DividerResult result = divider.divides(dividend, divisor);
-		assertEquals(Integer.valueOf(5), result.getResult());
-		assertEquals(Integer.valueOf(10), result.getDivident());
-		assertEquals(Integer.valueOf(2), result.getDivisor());
-		
-		
+		DividerResult result;
+		try {
+			result = divider.divides(dividend, divisor);
+			fail("dovrebbe lanciare un'ecezzione e non la fatto");
+
+		} catch (ImpossibleCalculationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
-	
-	
-	
-	
 
-}
+	@Test(expected = ImpossibleCalculationException.class)
+	public void testDivides_divisionByZero2() throws ImpossibleCalculationException {
+		Integer dividend = 10;
+		Integer divisor = 0;
+		DividerResult result;
+		result = divider.divides(dividend, divisor);
+	
+	}
+
+	}
+
+
