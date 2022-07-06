8a921aa2db28239d4a886fea32215dd4db96005e
LAPTOP-51345M00\hatim <hatimmes72@gmail.com>
2022-07-06 12:55:54 +0200

    Esercizio 1 mesrar Hatim

---
+++ src/main/java/eu/afea/training/mockito/exercise/Divider.java
@@ -0,0 +1,14 @@
+package eu.afea.training.mockito.exercise;
+
+public class Divider {
+
+	public Divider() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int Div(int i, int j) {
+		// TODO Auto-generated method stub
+		return i/j;
+	}
+
+}
--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -1,5 +1,23 @@
 package eu.afea.training.mockito.exercise;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class DividerTest {
+	private Divider divider;
+
+	@Before
+	
+	public void setup() {
+		this.divider = new Divider();
+		
+	}
+	@Test
+	public void testdIV()
+	{
+		assertEquals("1/1 != 1 ", 1, this.divider.Div(1,1));
+	}
 
 }
