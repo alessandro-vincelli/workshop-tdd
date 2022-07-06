03921876089fb762405c1ab3652c5bc194397729
FLIPPY\Francesco <francyspanu01@gmail.com>
2022-07-06 15:13:17 +0200

    no message

--- src/main/java/eu/afea/training/mockito/exercise/Divider.java
+++ src/main/java/eu/afea/training/mockito/exercise/Divider.java
@@ -2,6 +2,7 @@ package eu.afea.training.mockito.exercise;
 
 public class Divider {
 	
+	
 	public Division_result divides(int divided1, int diveded2) {
 		Division_result d = new Division_result(divided1, diveded2, divided1/diveded2);
 		d.setDiv1(divided1);
