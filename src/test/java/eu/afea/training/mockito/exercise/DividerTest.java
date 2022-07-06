7601eada7730ae68ff463c1405dc4020a23db389
LAPTOP-51345M00\hatim <hatimmes72@gmail.com>
2022-07-06 17:28:05 +0200

    Esercizio 2 con il throw

--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -3,6 +3,7 @@ package eu.afea.training.mockito.exercise;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class DividerTest {
@@ -26,6 +27,7 @@ public class DividerTest {
 		assertEquals(Integer.valueOf(2), ris.getDiv2());
 		
 	}
+	/*
 	@Test
 	public void Divisione_per_zero() throws ImpossibleClalculationException
 	{
@@ -37,6 +39,37 @@ public class DividerTest {
 		assertEquals(Integer.valueOf(10), ris.getDiv1());
 		assertEquals(Integer.valueOf(0), ris.getDiv2()); 
 		
+		}
+	*/
+	@Test(expected = ImpossibleClalculationException.class)
+	public void Divisione_per_zero() throws ImpossibleClalculationException {
+		Integer div1 = 10;
+		Integer div2 = 0;
+		DivisionResult ris = divider.Div(div1,div2);
+			assertEquals(Integer.valueOf(10), ris.getDiv1());
+			assertEquals(Integer.valueOf(0), ris.getDiv2());
+	 //int result = calculator.squareRoot(-1);
+	}
+	// Test is skipped:
+
+
+	/*
+	@Test
+	public void testSquareRoot_negativeArgument() {
+	 try {
+		 	Integer div1 = 10;
+			Integer div2 = 0;
+		 DivisionResult ris = divider.Div(div1,div2);
+		 assertEquals(Integer.valueOf(10), ris.getDiv1());
+		 assertEquals(Integer.valueOf(0), ris.getDiv2()); 
+	 //int result = calculator.squareRoot(-1);
+	 //fail(“Should throw IllegalArgumentException”);
+	 } 
+	 catch(IllegalArgumentException e) {
+	 // Additional assertions...
+		 
+	 }
+	 */
 	}
 		/*
 		DivisionResult R = new DivisionResult(10,30,89);
@@ -48,4 +81,4 @@ public class DividerTest {
 	
 	
 
-}
+
