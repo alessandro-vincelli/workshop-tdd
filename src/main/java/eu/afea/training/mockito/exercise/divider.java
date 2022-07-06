49ba3946d01aa444eba612d6f8b3356583cfcb1e
LAPTOP-9FMPG570\39328 <Damio9422@hotmail.it>
2022-07-06 17:51:45 +0200

    Esercizio2

--- src/main/java/eu/afea/training/mockito/exercise/divider.java
+++ src/main/java/eu/afea/training/mockito/exercise/divider.java
@@ -2,14 +2,15 @@ package eu.afea.training.mockito.exercise;
 
 public class divider {
 
-	
-	
-	
-	
-	public DivisionResult divides(Integer dividend, Integer divisor) {
-		
+	public DivisionResult divides(Integer dividend, Integer divisor) throws ImpossibleCalculationException {
+		try {
 		return new DivisionResult(dividend,divisor, dividend/divisor);	
-	
-	}
+	    
+		} catch (Exception e) {
+			throw new ImpossibleCalculationException();
 
+
+		}
+
+	}
 }
--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -1,43 +1,76 @@
 package eu.afea.training.mockito.exercise;
 
-
 import static org.junit.Assert.*;
 
-
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class DividerTest {
-	
+
 	private divider divider;
-	
+
 	@Before
 	public void setupTest() {
 		divider = new divider();
-		
-		
+
 	}
-	
+
 	@Test
-	public void testDivides() {
+	public void testDivides() throws ImpossibleCalculationException {
 		Integer dividend = 10;
 		Integer divisor = 2;
-		
-		DivisionResult result = divider.divides(dividend, divisor);
-		
-		assertEquals(Integer.valueOf(5), result.getResult());
-		assertEquals(Integer.valueOf(10), result.getResult());
-		assertEquals(Integer.valueOf(2), result.getResult());
 
-		
-		
-		
-	
+		DivisionResult result = divider.divides(dividend, divisor);
+
+		assertEquals(Integer.valueOf(5), result.getResult());
+		assertEquals(Integer.valueOf(dividend), result.getDividend());
+		assertEquals(Integer.valueOf(divisor), result.getDivisor());
 
 	}
-	
+    @Ignore("Solo per vedere")
+	@Test
+	public void testDivision_ByZero() throws ImpossibleCalculationException {
+
+		Integer dividend = 10;
+		Integer divisor = 0;
+
+		DivisionResult result = divider.divides(dividend, divisor);
+
+		assertEquals(Integer.valueOf(5), result.getResult());
+		assertEquals(Integer.valueOf(dividend), result.getDividend());
+		assertEquals(Integer.valueOf(divisor), result.getDivisor());
+
+	}
+
+	@Test
+	public void testDivision_ByZero2() throws ImpossibleCalculationException {
+		try {
+			Integer dividend = 10;
+			Integer divisor = 0;
+
+			DivisionResult result = divider.divides(dividend, divisor);
+
+			assertEquals(Integer.valueOf(5), result.getResult());
+			assertEquals(Integer.valueOf(dividend), result.getDividend());
+			assertEquals(Integer.valueOf(divisor), result.getDivisor());
+			fail("Should throw IllegalArgumentException");
+		} catch (ImpossibleCalculationException e) {
+
+		}
+		
+	}
+		@Test(expected = ImpossibleCalculationException.class)
+		public void testDivision_ByZero3() throws ImpossibleCalculationException {
+
+			Integer dividend = 10;
+			Integer divisor = 0;
+
+			DivisionResult result = divider.divides(dividend, divisor);
+
+			assertEquals(Integer.valueOf(5), result.getResult());
+			assertEquals(Integer.valueOf(dividend), result.getDividend());
+			assertEquals(Integer.valueOf(divisor), result.getDivisor());
+	}
+
 }
-
-
-
-
