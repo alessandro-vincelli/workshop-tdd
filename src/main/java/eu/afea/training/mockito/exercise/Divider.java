75795f745ab051fed8ff9c00fb3d66d67bdd762a
FLIPPY\Francesco <francyspanu01@gmail.com>
2022-07-07 10:46:55 +0200

    TestroundUp_VersioneBest(commento Math.cail() )

--- src/main/java/eu/afea/training/mockito/exercise/Divider.java
+++ src/main/java/eu/afea/training/mockito/exercise/Divider.java
@@ -8,15 +8,15 @@ public class Divider {
 			Division_result d = new Division_result(divided1, diveded2);
 			d.setDiv1(divided1);
 			d.setDiv2(diveded2);
+			/*
 			double r = (double)(divided1)/(double)(diveded2);
 			double a = Math.ceil(r);
 			d.setResult((int)a);
-			/*
+			*/
 			if(divided1%diveded2==1)
 				d.setResult((divided1/diveded2)+1);
 			else
 				d.setResult(divided1/diveded2);
-			*/
 			return d;
 		}
 		catch(ArithmeticException e) {
