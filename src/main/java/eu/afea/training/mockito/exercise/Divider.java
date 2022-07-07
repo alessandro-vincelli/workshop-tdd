2f0b806e9e271caf0ff98d156935b510d034bf6f
LAPTOP-51345M00\hatim <hatimmes72@gmail.com>
2022-07-07 12:07:59 +0200

    Esercizio 4 Hatim Mesrar

--- src/main/java/eu/afea/training/mockito/exercise/Divider.java
+++ src/main/java/eu/afea/training/mockito/exercise/Divider.java
@@ -1,7 +1,10 @@
 package eu.afea.training.mockito.exercise;
 
-public class Divider {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Divider {
+	private static List<DivisionResult> ListaDiv = new ArrayList<>();
 	public Divider() {
 		// TODO Auto-generated constructor stub
 	}
@@ -9,10 +12,11 @@ public class Divider {
 	public  DivisionResult Div(Integer div1, Integer div2) throws ImpossibleClalculationException {
 		// TODO Auto-generated method stub
 		//DivisionResult D = new DivisionResult();
+		 
 		if(div2 !=0)
 			
 		 {
-	        
+	      
 	        System.out.println("Try Block -->  Start");
 	       //return Division(div1, div2, div1/div2)  iniziale prima della modifica 
 	        
@@ -21,13 +25,17 @@ public class Divider {
 	       ris =  Math.ceil(ris);
 	        
 	        DivisionResult divisionResult = new DivisionResult(div1, div2, (int) ris);
-	        
+	         ListaDiv.add(divisionResult);
 	        return divisionResult;
 	        
 	     }
 		else { 
+			
+			 DivisionResult divisionResult = new DivisionResult(div1, div2, null);
+	        ListaDiv.add(divisionResult);
 	        throw new ImpossibleClalculationException();
-	    
+	        
+	      
 	     }
 		//return new DivisionResult(div1, div2, div1/div2);
 	}
