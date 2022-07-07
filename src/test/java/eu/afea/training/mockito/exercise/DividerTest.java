56a61b83ffb103e1cf09db4c6ea0323f05ebbadf
LAPTOP-51345M00\hatim <hatimmes72@gmail.com>
2022-07-07 09:50:52 +0200

    esercizio 3 Hatim Mesrar

--- src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
+++ src/test/java/eu/afea/training/mockito/exercise/DividerTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class DividerTest {
 	private Divider divider;
 
-	@Before
+	@Before //serve per istanziare per ogni test così da farli ogni volta ciascuno e ripetere il procedimento
 	
 	public void setup() {
 		this.divider = new Divider();   //per istanziare e non ripetere per ogni testdIV
@@ -28,7 +28,7 @@ public class DividerTest {
 		assertEquals(Integer.valueOf(2), ris.getDiv2());
 		
 	}
-	@Ignore("solo per Studiare")
+	@Ignore("solo per Studiarci sopra perchè era una passaggio intermedio")
 	@Test
 	public void Divisione_per_zero() throws ImpossibleClalculationException
 	{
@@ -60,8 +60,8 @@ public class DividerTest {
 	@Test
 	public void Divisione_per_zero3() throws ImpossibleClalculationException {
 	 try {
-		 	Integer div1 = 10;
-			Integer div2 = 0;
+		 	Integer div1 = 3;
+			Integer div2 = 2;
 			DivisionResult ris = divider.Div(div1,div2);
 		 //assertEquals(Integer.valueOf(10), ris.getDiv1());
 		// assertEquals(Integer.valueOf(0), ris.getDiv2()); 
@@ -70,12 +70,32 @@ public class DividerTest {
 	 } 
 	 catch(ImpossibleClalculationException e) {
 	 // Additional assertions...
-	      
-
-		 
+ 
 	 }
 	 
+	 
 	}
+	
+	@Test
+	public void divisione32() throws ImpossibleClalculationException{
+		 try {
+			 	Integer div1 = 3;
+				Integer div2 = 2;
+				DivisionResult ris = divider.Div(div1,div2);
+			 //assertEquals(Integer.valueOf(10), ris.getDiv1());
+			// assertEquals(Integer.valueOf(0), ris.getDiv2()); 
+		 //int result = calculator.squareRoot(-1);
+			 fail("Should throw ImpossibleClalculationException");
+		 } 
+		 catch(ImpossibleClalculationException e) {
+		 // Additional assertions...
+	 
+		 }
+		
+	}
+	
+	
+	
 }
 	 
 		
